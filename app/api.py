d15751d2d9a7239ed413e0a6a94df0cba83bef15
NxtTon <142604053+NxtTon@users.noreply.github.com>
2024-09-04 16:07:10 -0300

    feat: ✨ remoção dos comentarios.

--- app/api.py
+++ app/api.py
@@ -5,13 +5,12 @@ from app.routes import router
 
 app = FastAPI()
 
-# Inclui as rotas definidas em routes.py
 app.include_router(router)
 
-# Monta o diretório static para servir arquivos estáticos
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Define a rota para a página inicial
+
 @app.get("/")
 async def read_index():
     return FileResponse("static/index.html")
