ba4d85231978b3096e3585a5ae062b621d80498b
NxtTon <142604053+NxtTon@users.noreply.github.com>
2024-09-18 17:33:46 -0300

    refactor: ♻️ remoção dos comentarios.

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,5 @@
-# from fastapi import APIRouter, Form
-# from .YouTubeScraper import YouTubeScraper, robo_get_video_id
-# from .main import YouTubeCommentExtractor
-# from dotenv import load_dotenv
-# import os
-
-# load_dotenv()
-
-# router = APIRouter()
-# api_key = os.getenv('API_KEY')
-# file_path = os.getenv('FILE_PATH')
-
-# @router.post("/pesquisar")
-# async def receber_texto(pesquisa: str = Form(...)):
-#     scraper = YouTubeScraper()
-
-#     video_id = robo_get_video_id(pesquisa)
-
-#     extractor = YouTubeCommentExtractor(api_key, video_id)
-#     extractor.run(file_path)
-#     return {"texto_recebido": pesquisa}
 from fastapi import APIRouter, Form
-from .YouTubeScraper import YouTubeScraper, robo_get_video_id
+from .YouTubeScraper import  robo_get_video_id
 from .main import YouTubeCommentExtractor
 from .sentiment_analysis import analyze_sentiments
 from dotenv import load_dotenv
